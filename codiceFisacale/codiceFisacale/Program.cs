469327734ca47e852295b5dbc142c5d2fddd254d
cesarecucchiarini <cucchiarini.cesare@franchettisalviani.net>
2025-02-19 09:59:21 +0100

    inizio
    mancano gli ultimi 5 catratteri

---
+++ codiceFisacale/codiceFisacale/Program.cs
@@ -0,0 +1,111 @@
+﻿//VOID
+void creaCodice(string cognome, ref string codice,string nome, string anno, string mese,int giorno, string sesso, string comune)
+{
+    string vocali = "AEIOU", nomeControllo="", lettere="ABCDEHLMPRST", percorso="../../../../comuni.txt";
+    string[] mesi = { "gennaio", "febbraio", "marzo", "aprile", 
+        "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"};
+    //COGNOME
+    for(int i = 0; i < cognome.Length && codice.Length<3; i++)
+    {
+        if (!vocali.Contains(cognome[i]))
+        {
+            codice += cognome[i];
+        }
+    }
+    for(int i=0;i<cognome.Length && codice.Length < 3; i++)
+    {
+        if (vocali.Contains(cognome[i]))
+        {
+            codice += cognome[i];
+        }
+    }
+    while(codice.Length < 3)
+    {
+        codice += 'X';
+    }
+    //NOME
+    for(int i=0;i<nome.Length && nomeControllo.Length <=4; i++)
+    {
+        if (!vocali.Contains(nome[i]))
+        {
+            nomeControllo+= nome[i];
+        }
+    }
+    if(nomeControllo.Length >= 4)
+    {
+        codice += nome[0] + nome.Substring(2);
+    }
+    else
+    {
+        codice += nomeControllo;
+    }
+    for(int i=0; i < nome.Length && codice.Length < 6; i++)
+    {
+        if (vocali.Contains(nome[i]))
+        {
+            codice += nome[i];
+        }
+    }
+    while (codice.Length < 6)
+    {
+        codice += 'X';
+    }
+    //ANNO
+    codice += anno.Substring(anno.Length - 2);
+    //MESE
+    for(int i = 0; i < mesi.Length; i++)
+    {
+        if (mese == mesi[i])
+        {
+            codice += lettere[i];
+            i = 13;
+        }
+    }
+    //GIORNO
+    if (sesso == "M")
+    {
+        codice += giorno.ToString();
+    }
+    else
+    {
+        codice += (giorno + 40).ToString();
+    }
+    //COMUNE
+    string fileCompleto=File.ReadAllText(percorso);
+    string[] codici = fileCompleto.Split(';');
+    for(int i = 5; i < codici.Length; i += 25)
+    {
+        if (comune.ToUpper() == codici[i].ToUpper())
+        {
+            codice += codici[i + 12];
+        }
+    }
+}
+//MAIN
+string codice="";
+Console.WriteLine("Dammi il cognome");
+string cogn = Console.ReadLine();
+cogn=cogn.ToUpper().Replace(" ", "").Replace("'", "");
+
+Console.WriteLine("Dammi il nome (se ne hai più di uno mettili tutti separati da uno spazio)");
+string nome=Console.ReadLine();
+nome = nome.Replace(" ", "").ToUpper();
+
+Console.WriteLine("Dammi il tuo anno di nascita");
+string anno = Console.ReadLine();
+
+Console.WriteLine("Dimmi il mese di nascita");
+string mese=Console.ReadLine();
+
+Console.WriteLine("Dammi il giorno di nascita");
+int giorno = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Dimmi il tuo sesso (M o F)");
+string sesso=Console.ReadLine();
+sesso = sesso.ToUpper();
+
+Console.WriteLine("Dimmi il comune di nascita");
+string comune=Console.ReadLine();
+
+creaCodice(cogn, ref codice,nome, anno, mese.ToLower(), giorno, sesso, comune);
+Console.WriteLine($"Il tuo codice fiscale è {codice}");
